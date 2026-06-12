9714e6428f08747d89028eaacae68f8a84e70e38
Techwithdiwana <techwithdiwana@gmail.com>
2026-06-12 12:30:36 +0530

    Add ConfigMap and Secret support for auth service

--- auth-service/app/main.py
+++ auth-service/app/main.py
@@ -1,19 +1,28 @@
 from fastapi import FastAPI
+import os
 
 app = FastAPI()
+
+DB_HOST = os.getenv("DB_HOST", "not-set")
+DB_USER = os.getenv("DB_USER", "not-set")
+
 
 @app.get("/health")
 def health():
     return {
         "status": "UP",
-        "service": "auth-service"
+        "service": "auth-service",
+        "db_host": DB_HOST,
+        "db_user": DB_USER
     }
+
 
 @app.post("/login")
 def login():
     return {
         "message": "Login Successful"
     }
+
 
 @app.post("/register")
 def register():
