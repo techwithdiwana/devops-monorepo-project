1cda5399c68ed72afe7630bebfd7f54a0b434a4e
Techwithdiwana <techwithdiwana@gmail.com>
2026-06-21 22:52:27 +0530

    added file

--- auth-service/app/main.py
+++ auth-service/app/main.py
@@ -14,7 +14,10 @@ DB_PORT = int(os.getenv("DB_PORT", "3306"))
 DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
-
+DATABASE_URL = (
+    f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}"
+    f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
 
 @app.get("/health")
 def health():
